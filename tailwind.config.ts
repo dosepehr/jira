86cc0c296db40d6b6d2a071977f699c8ce9be19c
dosepehr <dosepehr@gmail.com>
2024-12-05 17:24:09 +0330

    add tailwind plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
-import type { Config } from "tailwindcss";
-
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
+import { PluginCreator } from 'tailwindcss/types/config';
+import ppt from 'prettier-plugin-tailwindcss';
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: [daisyui, ppt as unknown as PluginCreator]
 };
 export default config;
