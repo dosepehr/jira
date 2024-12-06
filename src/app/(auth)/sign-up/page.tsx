8f4e10d9aeda62a36936398509066e678b4d6928
dosepehr <dosepehr@gmail.com>
2024-12-06 14:39:25 +0330

    add auth links

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -5,28 +5,33 @@ const page = () => {
     return (
         <div className='bg-white max-w-2xl mx-auto rounded-md p-4'>
             <p className='text-black font-bold text-2xl text-center p-4'>
-                Welcome back!
+                Sign up
             </p>
             <hr className='mb-4' />
             <form className='flex flex-col mx-auto gap-y-4 items-center max-w-sm'>
                 <input
                     type='text'
+                    placeholder='Enter your name'
+                    className='input input-bordered w-full  bg-white text-gray-600'
+                />
+                <input
+                    type='text'
                     placeholder='Enter email address'
-                    className='input input-bordered w-full  bg-white text-gray-400'
+                    className='input input-bordered w-full  bg-white text-gray-600'
                 />
                 <input
                     type='text'
                     placeholder='Enter password'
-                    className='input input-bordered w-full  bg-white text-gray-400'
+                    className='input input-bordered w-full  bg-white text-gray-600'
                 />
                 <button className='btn btn-primary text-white w-full'>
-                    Login
+                    sign up
                 </button>
             </form>
             <div className='flex flex-col items-center gap-y-2 mt-4'>
-                <p className='text-gray-600'>don&apos;t have an account?</p>
-                <Link className='text-primary font-semibold' href={'/sign-up'}>
-                    register
+                <p className='text-gray-600'>Already have an account?</p>
+                <Link className='text-primary font-semibold' href={'/sign-in'}>
+                    login
                 </Link>
             </div>
         </div>
