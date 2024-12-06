b915418c5c98f1260f93a833f8890084191e9759
dosepehr <dosepehr@gmail.com>
2024-12-06 13:03:28 +0330

    add tailwind screen configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,16 +3,32 @@ import daisyui from 'daisyui';
 const config: Config = {
     content: [
         './src/components/**/*.{js,ts,jsx,tsx}',
-        './src/app/**/*.{js,ts,jsx,tsx,}'
+        './src/app/**/*.{js,ts,jsx,tsx,}',
     ],
     theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                '2xl': '1200px',
+            },
+        },
         extend: {
+            screens: {
+                xs: '320px',
+                sm: '576px',
+                md: '768px',
+                lg: '992px',
+                xl: '1200px',
+                '2xl': '1400px'
+            },
             colors: {
                 background: 'var(--background)',
-                foreground: 'var(--foreground)'
-            }
-        }
+                foreground: 'var(--foreground)',
+            },
+        },
     },
-    plugins: [daisyui]
+    plugins: [daisyui],
 };
 export default config;
+
